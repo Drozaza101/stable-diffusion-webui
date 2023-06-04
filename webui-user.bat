52a1f8adcd1135c411350f379f312a3944e01a11
Drozaza101 <sandro12345kuratli@gmail.com>
2023-06-04 14:14:46 +0200

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\sandr\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--lowvram --precision full --no-half --skip-torch-cuda-test
 
 call webui.bat
